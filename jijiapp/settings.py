101d6aed38b62550d1b05869ef7318d9dc609211
KudusMusah <118946022+KudusMusah@users.noreply.github.com>
2024-06-11 21:41:26 +0000

    Update settings.py

--- jijiapp/settings.py
+++ jijiapp/settings.py
@@ -91,7 +91,11 @@ WSGI_APPLICATION = 'jijiapp.wsgi.application'
 # }
 
 DATABASES={
-    'default':dj_database_url.parse("postgres://jiji_db_user:sy6DJKb78onHIIX7MmWHPR4HLTGilaaU@dpg-cpk99amct0pc73b4sm30-a.oregon-postgres.render.com/jiji_db")
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgres://jiji_db_user:sy6DJKb78onHIIX7MmWHPR4HLTGilaaU@dpg-cpk99amct0pc73b4sm30-a.oregon-postgres.render.com/jiji_db',
+        conn_max_age=600
+    )
 }
 
 
