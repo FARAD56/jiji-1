bdd6f506f97dfba065292a31da58e00d7ac93725
KudusMusah <118946022+KudusMusah@users.noreply.github.com>
2024-06-11 21:56:37 +0000

    Update settings.py

--- jijiapp/settings.py
+++ jijiapp/settings.py
@@ -98,19 +98,25 @@ WSGI_APPLICATION = 'jijiapp.wsgi.application'
 #     )
 # }
 
+# DATABASES = {
+#     'default': {
+#         #'ENGINE': 'django.db.backends.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'jsJdmeDpoEXVONKxlZqGJKkXrVSUsoBY',
+#         'HOST': 'roundhouse.proxy.rlwy.net',
+#         'PORT': '45544',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'jsJdmeDpoEXVONKxlZqGJKkXrVSUsoBY',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '45544',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
